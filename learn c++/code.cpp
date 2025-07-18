b0622e7d9f5ad20229ad6cb3e921f76274a916b0
Umang <umangsharma1189@gmail.com>
2025-07-18 16:50:00 +0530

    second day commit

--- learn c++/code.cpp
+++ learn c++/code.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
-using namespace std;
+#include <iostream>. 
+using namespace std;    // both lines are important to write no matter what css file
 
 int main() {
     cout <<"umang"<<endl<<"sharma";
-    return 0;
+    return 0;  // a good practice to write/
 }
+// Above is called boiler plate code
---
+++ learn c++/typeconv.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float meow = 3.99f;
+    double bow = meow;
+    cout <<meow<<endl<<bow;
+}
